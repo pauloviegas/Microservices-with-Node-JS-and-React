5f679df7e0da5de44343c2a0fdeaebf756c1e94a
Paulo Viegas <paulo.viegas@hydradev.io>
2023-05-18 11:21:06 +0100

    FIX: remove required authentication

--- ticketing/auth/src/routes/currentUser.ts
+++ ticketing/auth/src/routes/currentUser.ts
@@ -1,11 +1,10 @@
 import exprress from "express";
 
 import { currentUser } from "../middlewares/currentUser";
-import { requireAuth } from "../middlewares/requireAuth";
 
 const router = exprress.Router();
 
-router.get("/api/users/currentuser", currentUser, requireAuth, (req, res) => {
+router.get("/api/users/currentuser", currentUser, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 });
 
